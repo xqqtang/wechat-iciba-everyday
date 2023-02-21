29eb5bdb9d92e197b2b9818a2ff5e044fdf23a7f
xqqtang <125367276+xqqtang@users.noreply.github.com>
2023-02-21 14:10:12 +0800

    Update main.py

--- py3/main.py
+++ py3/main.py
@@ -5,14 +5,14 @@ import Iciba
 if __name__ == '__main__':
     # 微信配置
     wechat_config = {
-        'appid': 'xxxxx', #(No.1)此处填写公众号的appid
-        'appsecret': 'xxxxx', #(No.2)此处填写公众号的appsecret
-        'template_id': 'xxxxx' #(No.3)此处填写公众号的模板消息ID
+        'appid': os.environ["APP_ID"] , #(No.1)此处填写公众号的appid
+        'appsecret': os.environ["APP_SECRET"] , #(No.2)此处填写公众号的appsecret
+        'template_id': os.environ["TEMPLATE_ID"]  #(No.3)此处填写公众号的模板消息ID
     }
     
     # 用户列表
     openids = [
-        'xxxxx', #(No.4)此处填写你的微信号（微信公众平台上的微信号）
+        os.environ["USER_ID"], #(No.4)此处填写你的微信号（微信公众平台上的微信号）
         #'xxxxx', #如果有多个用户也可以
         #'xxxxx',
     ]
